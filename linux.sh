0bc8fedc498278983c5d160c93a4d6cb01b1fa16
dzshzx <dzshzx0930+github@gmail.com>
2026-05-15 17:44:41 +0800

    ci: align release packaging scripts

--- linux.sh
+++ linux.sh
@@ -1,8 +1,12 @@
+# shellcheck shell=sh
+
 version=$(head -n 5 pubspec.yaml | tail -n 1 | cut -d ' ' -f 2)
+dest="${HOME}/Desktop/JHenTai_${version}"
 
 flutter build linux --release -t lib/src/main.dart \
-&& mkdir ~/Desktop/JHenTai_${version} \
-&& cp -r build/linux/x64/release/bundle/* ~/Desktop/JHenTai_${version}/ \
-&& cd ~/Desktop \
-&& zip -ro JHenTai_${version}.zip JHenTai_${version} \
-&& rm -rf mkdir ~/Desktop/JHenTai_${version}
+&& rm -rf "$dest" \
+&& mkdir -p "$dest" \
+&& cp -r build/linux/x64/release/bundle/* "$dest"/ \
+&& cd "${HOME}/Desktop" \
+&& zip -ro "JHenTai_${version}.zip" "JHenTai_${version}" \
+&& rm -rf "$dest"
--- windows.sh
+++ windows.sh
@@ -1,7 +1,12 @@
-version=$(head -n 5 pubspec.yaml | tail -n 1 | cut -d ' ' -f 2)
+# shellcheck shell=sh
 
-flutter build windows -t lib/src/main.dart \
-&& cp -r build/windows/runner/Release/ ~/Desktop/JHenTai_${version}_windows/ \
-&& cd ~/Desktop \
-&& zip -ro JHenTai_${version}_windows.zip JHenTai_${version}_windows \
-&& rm -rf JHenTai_${version}_windows
+version=$(head -n 5 pubspec.yaml | tail -n 1 | cut -d ' ' -f 2)
+dest="${HOME}/Desktop/JHenTai_${version}_windows"
+
+flutter build windows --release -t lib/src/main.dart \
+&& rm -rf "$dest" \
+&& mkdir -p "$dest" \
+&& cp -r build/windows/x64/runner/Release/* "$dest"/ \
+&& cd "${HOME}/Desktop" \
+&& zip -ro "JHenTai_${version}_windows.zip" "JHenTai_${version}_windows" \
+&& rm -rf "$dest"
